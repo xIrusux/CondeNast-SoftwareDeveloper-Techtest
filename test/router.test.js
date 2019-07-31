6793a4f1c83c7bb7d7eb50fd31a4b8d774729ee9
Christine <christine.altmann@gmx.com>
2019-07-31 16:20:42 +0100

    test for router to return status code 200

--- test/router.test.js
+++ test/router.test.js
@@ -7,3 +7,14 @@ tape("Initialise", t => {
   t.equal(num, 2, "should equal 2");
   t.end();
 });
+
+tape("Home route returns a status code of 200", t => {
+  supertest(router)
+    .get("/")
+    .expect(200)
+    .expect("Content-Type", /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.end();
+    });
+});
