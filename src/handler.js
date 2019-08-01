1da2e969035f7c770ac18a64699d4e0fdc1f160e
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-08-01 10:38:34 +0100

    TEST 3: Search returns a status code of 200 and any file in public folder

--- src/handler.js
+++ src/handler.js
@@ -5,9 +5,25 @@ let handleHome = (request, response) => {
   response.end("Hi");
 };
 
+let handlePublic = (request, response, endpoint) => {
+  const fileType = endpoint.split(".")[1];
+  const extensions = {
+    html: "text/html",
+    css: "text/css",
+    js: "application/javascript",
+    ico: "image/x-icon",
+    jpeg: "image/jpeg",
+    jpg: "image/jpeg",
+    png: "image/png"
+  };
+
+  response.writeHead(200, { "Content-Type": extensions[fileType] });
+  response.end();
+};
+
 let handleApi = (request, response, endpoint) => {
   response.writeHead(200, { "Content-Type": "application/json" });
   response.end("");
 };
 
-module.exports = { handleHome, handleApi };
+module.exports = { handleHome, handlePublic, handleApi };
--- src/router.js
+++ src/router.js
@@ -1,11 +1,14 @@
-const { handleHome, handleApi } = require("../src/handler");
+const { handleHome, handlePublic, handleApi } = require("../src/handler");
 
 const router = (request, response) => {
   const endpoint = request.url;
   if (endpoint === "/") {
     handleHome(request, response);
+  } else if (endpoint.indexOf("public") !== -1) {
+    console.log(endpoint.indexOf("public"));
+    handlePublic(request, response, endpoint);
   } else if (endpoint.includes("/search")) {
-      handleApi(request, response, endpoint)
+    handleApi(request, response, endpoint);
   }
 };
 
